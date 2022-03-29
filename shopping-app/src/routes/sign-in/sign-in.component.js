f18b3be01605a8fe1ef23700c7c5f377d60e1616
James <james-maccarthy@hotmail.com>
2022-03-29 16:06:29 +0100

    firebase google user authentification added

--- shopping-app/src/routes/sign-in/sign-in.component.js
+++ shopping-app/src/routes/sign-in/sign-in.component.js
@@ -1,8 +1,17 @@
+import { signInWithGooglePopUp } from "../../utils/firebase/firebase";
+
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopUp();
+        console.log(response);
+    }
     return (
         <div>
             This is the sign in page
-        
+            hello
+            <button>button</button>
+            <button onClick={logGoogleUser}> sign in with google </button>
         </div>
     )
 }
---
+++ shopping-app/src/utils/firebase/firebase.js
@@ -0,0 +1,24 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth, signInWithRirect, signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeKeAU2EwPpHT-xL3b2jDdGwKfeVeqaN4",
+    authDomain: "crown-clothing-db-804c8.firebaseapp.com",
+    projectId: "crown-clothing-db-804c8",
+    storageBucket: "crown-clothing-db-804c8.appspot.com",
+    messagingSenderId: "864123458674",
+    appId: "1:864123458674:web:b8fe78b3c8f3a8bec85eeb"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+      prompt: 'select_account'
+
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopUp = ()=> signInWithPopup(auth, provider);
